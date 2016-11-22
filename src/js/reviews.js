f2bad883d2793446eecd7f69b226e03a8fa2ad2c
Paintrain86 <webmaster.karpov@yandex.ru>
2016-11-22 21:58:46 +0300

    module7-task1

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,11 +1,40 @@
 'use strict';
-var REVIEW_LIST_URL = 'http://localhost:1507/api/reviews';
 
 define([
   './load',
   './renderReviews',
 ], function(load, renderReviews) {
-  return function() {
-    load(REVIEW_LIST_URL, renderReviews);
+  var REVIEW_LIST_URL = '/api/reviews';
+
+  var activeFilter = 'reviews-all';
+  var reviewsContainer = document.querySelector('.reviews-list');
+  var reviewsFilter = document.querySelector('.reviews-filter');
+  var pageActive = 0;
+  var pageSize = 3;
+
+  var loadReviews = function(filter, pageCurrent) {
+    load(REVIEW_LIST_URL, {
+      from: pageCurrent * pageSize,
+      to: pageCurrent * pageSize,
+      filter: filter
+    }, renderReviews);
   };
+
+  var changeFilters = function(filterID) {
+    reviewsContainer.innerHtml = '';
+    activeFilter = (typeof filterID === 'undefined') ? activeFilter : filterID;
+    pageActive = 0;
+    loadReviews(activeFilter, pageActive);
+  };
+
+  reviewsFilter.addEventListener('click', function(e) {
+    if (e.target.classList.contains('reviews-filter-item')) {
+      if (e.target.id !== activeFilter) {
+        changeFilters(e.target.id);
+      }
+    }
+  });
+  // var lastCall = Date.now();
+
+  return changeFilters;
 });

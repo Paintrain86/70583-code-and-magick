c9fb96133b35e56f8d8483569b7ab6cd54bab21b
Paintrain86 <webmaster.karpov@yandex.ru>
2016-11-22 12:36:29 +0300

    render Review function now returns in define

--- src/js/renderReviews.js
+++ src/js/renderReviews.js
@@ -1,71 +1,70 @@
 'use strict';
-var reviews = [];
+define([
+  './review'
+], function(Review) {
+  var reviews = [];
 
-var reviewsFilterBlock = document.querySelector('.reviews-filter'),
-  reviewsList = document.querySelector('.reviews-list');
-var IMAGE_LOAD_TIMEOUT = 3000;
+  var reviewsFilterBlock = document.querySelector('.reviews-filter'),
+    reviewsList = document.querySelector('.reviews-list');
+  var IMAGE_LOAD_TIMEOUT = 3000;
 
-function toggleFilterBlock(action) {
-  if (action === 'hide') {
-    reviewsFilterBlock.classList.add('invisible');
-  } else if (action === 'show') {
-    reviewsFilterBlock.classList.remove('invisible');
+  function toggleFilterBlock(action) {
+    if (action === 'hide') {
+      reviewsFilterBlock.classList.add('invisible');
+    } else if (action === 'show') {
+      reviewsFilterBlock.classList.remove('invisible');
+    }
   }
-}
 
-function getReviewItem(item) {
-  var reviewItem = new window.Review(item);
-  reviewItem.element.querySelector('.review-rating').textContent = item.rating;
-  reviewItem.element.querySelector('.review-text').textContent = item.description;
+  function getReviewItem(item) {
+    var reviewItem = new Review(item);
+    reviewItem.element.querySelector('.review-rating').textContent = item.rating;
+    reviewItem.element.querySelector('.review-text').textContent = item.description;
 
-  var img = new Image(),
-    imgTimeout = null;
-  img.onload = function(e) {
-    clearTimeout(imgTimeout);
-    var reviewImg = reviewItem.element.querySelector('.review-author');
+    var img = new Image(),
+      imgTimeout = null;
+    img.onload = function(e) {
+      clearTimeout(imgTimeout);
+      var reviewImg = reviewItem.element.querySelector('.review-author');
 
-    reviewImg.style.width = reviewImg.style.height = 124;
-    reviewImg.setAttribute('src', e.target.src);
-    reviewItem.element.querySelector('.review-author').setAttribute('title', item.author.name);
-  };
-  img.onerror = function() {
-    reviewItem.element.classList.add('review-load-failure');
-  };
-  img.src = item.author.picture;
+      reviewImg.style.width = reviewImg.style.height = 124;
+      reviewImg.setAttribute('src', e.target.src);
+      reviewItem.element.querySelector('.review-author').setAttribute('title', item.author.name);
+    };
+    img.onerror = function() {
+      reviewItem.element.classList.add('review-load-failure');
+    };
+    img.src = item.author.picture;
 
-  imgTimeout = setTimeout(function() {
-    img.src = '';
-    reviewItem.element.classList.add('review-load-failure');
-  }, IMAGE_LOAD_TIMEOUT);
+    imgTimeout = setTimeout(function() {
+      img.src = '';
+      reviewItem.element.classList.add('review-load-failure');
+    }, IMAGE_LOAD_TIMEOUT);
 
-  return reviewItem.element;
-}
+    return reviewItem.element;
+  }
 
-function renderReviews(items) {
-  reviews = items;
-  toggleFilterBlock('hide');
-  if (Array.isArray(reviews)) {
-    reviews.forEach(function(item) {
-      reviewsList.appendChild(getReviewItem(item));
-    });
-    if (reviewsList.children.length > 0) {
-      toggleFilterBlock('show');
+  function renderReviews(items) {
+    reviews = items;
+    toggleFilterBlock('hide');
+    if (Array.isArray(reviews)) {
+      reviews.forEach(function(item) {
+        reviewsList.appendChild(getReviewItem(item));
+      });
+      if (reviewsList.children.length > 0) {
+        toggleFilterBlock('show');
+      } else {
+        showNoReviewsMessage();
+      }
     } else {
       showNoReviewsMessage();
     }
-  } else {
-    showNoReviewsMessage();
   }
-}
 
-function showNoReviewsMessage() {
-  reviewsList.style.textAlign = 'center';
-  reviewsList.textContent = 'К этой классной игре пока никто не оставил ни одного отзыва :(';
-}
+  function showNoReviewsMessage() {
+    reviewsList.style.textAlign = 'center';
+    reviewsList.textContent = 'К этой классной игре пока никто не оставил ни одного отзыва :(';
+  }
 
-define([
-  './review'
-], function(review) {
-  window.Review = review;
   return renderReviews;
 });
